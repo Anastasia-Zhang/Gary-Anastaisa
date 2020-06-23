5acfc09eac957c41e97f55b54de1c11067f6e2d8
annaxyzhang <1003814986@qq.com>
2020-06-23 16:00:47 +0800

    分支合并测试2 ana

--- src/BranchTest.java
+++ src/BranchTest.java
@@ -2,7 +2,8 @@ public class BranchTest {
     public static void main(String[] args) {
         System.out.println("anastasia 分支原版");
         System.out.println("master 分支原版");
-        System.out.println("master 分支");
+        System.out.println("master 分支 ana分支添加");
+        System.out.println("这是 anastasia 分支特有的数据");
     }
 }
 
