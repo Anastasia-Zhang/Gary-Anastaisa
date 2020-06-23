5bfd433d2b901167dc7346b2549ecb6c64fba461
annaxyzhang <1003814986@qq.com>
2020-06-23 16:02:28 +0800

    分支合并测试2 mas

--- src/BranchTest.java
+++ src/BranchTest.java
@@ -2,7 +2,8 @@ public class BranchTest {
     public static void main(String[] args) {
         System.out.println("anastasia 分支原版");
         System.out.println("master 分支原版");
-        System.out.println("master 分支");
+        System.out.println("master 分支 master 改");
+        System.out.println("master 分支特有的数据");
     }
 }
 
